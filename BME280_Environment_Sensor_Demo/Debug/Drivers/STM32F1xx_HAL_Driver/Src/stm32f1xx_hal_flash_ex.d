a01be76a0bb9b8c0c1bc516c2e44b2365492134b
MicroArena <sshahryiar@users.noreply.github.com>
2023-04-15 12:48:59 +0600

    Add files via upload

---
+++ BME280_Environment_Sensor_Demo/Debug/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_flash_ex.d
@@ -0,0 +1,68 @@
+Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_flash_ex.o: \
+ ../Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_flash_ex.c \
+ C:/Users/sshah/Desktop/BME280_Environment_Sensor_Demo/BME280_demo/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal.h \
+ C:/Users/sshah/Desktop/BME280_Environment_Sensor_Demo/BME280_demo/Inc/stm32f1xx_hal_conf.h \
+ C:/Users/sshah/Desktop/BME280_Environment_Sensor_Demo/BME280_demo/Inc/main.h \
+ C:/Users/sshah/Desktop/BME280_Environment_Sensor_Demo/BME280_demo/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_rcc.h \
+ C:/Users/sshah/Desktop/BME280_Environment_Sensor_Demo/BME280_demo/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_def.h \
+ C:/Users/sshah/Desktop/BME280_Environment_Sensor_Demo/BME280_demo/Drivers/CMSIS/Device/ST/STM32F1xx/Include/stm32f1xx.h \
+ C:/Users/sshah/Desktop/BME280_Environment_Sensor_Demo/BME280_demo/Drivers/CMSIS/Device/ST/STM32F1xx/Include/stm32f103xe.h \
+ C:/Users/sshah/Desktop/BME280_Environment_Sensor_Demo/BME280_demo/Drivers/CMSIS/Include/core_cm3.h \
+ C:/Users/sshah/Desktop/BME280_Environment_Sensor_Demo/BME280_demo/Drivers/CMSIS/Include/core_cmInstr.h \
+ C:/Users/sshah/Desktop/BME280_Environment_Sensor_Demo/BME280_demo/Drivers/CMSIS/Include/cmsis_gcc.h \
+ C:/Users/sshah/Desktop/BME280_Environment_Sensor_Demo/BME280_demo/Drivers/CMSIS/Include/core_cmFunc.h \
+ C:/Users/sshah/Desktop/BME280_Environment_Sensor_Demo/BME280_demo/Drivers/CMSIS/Device/ST/STM32F1xx/Include/system_stm32f1xx.h \
+ C:/Users/sshah/Desktop/BME280_Environment_Sensor_Demo/BME280_demo/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_rcc_ex.h \
+ C:/Users/sshah/Desktop/BME280_Environment_Sensor_Demo/BME280_demo/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_gpio.h \
+ C:/Users/sshah/Desktop/BME280_Environment_Sensor_Demo/BME280_demo/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_gpio_ex.h \
+ C:/Users/sshah/Desktop/BME280_Environment_Sensor_Demo/BME280_demo/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_dma.h \
+ C:/Users/sshah/Desktop/BME280_Environment_Sensor_Demo/BME280_demo/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_dma_ex.h \
+ C:/Users/sshah/Desktop/BME280_Environment_Sensor_Demo/BME280_demo/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_cortex.h \
+ C:/Users/sshah/Desktop/BME280_Environment_Sensor_Demo/BME280_demo/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash.h \
+ C:/Users/sshah/Desktop/BME280_Environment_Sensor_Demo/BME280_demo/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash_ex.h \
+ C:/Users/sshah/Desktop/BME280_Environment_Sensor_Demo/BME280_demo/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_pwr.h \
+ C:/Users/sshah/Desktop/BME280_Environment_Sensor_Demo/BME280_demo/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_spi.h
+
+C:/Users/sshah/Desktop/BME280_Environment_Sensor_Demo/BME280_demo/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal.h:
+
+C:/Users/sshah/Desktop/BME280_Environment_Sensor_Demo/BME280_demo/Inc/stm32f1xx_hal_conf.h:
+
+C:/Users/sshah/Desktop/BME280_Environment_Sensor_Demo/BME280_demo/Inc/main.h:
+
+C:/Users/sshah/Desktop/BME280_Environment_Sensor_Demo/BME280_demo/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_rcc.h:
+
+C:/Users/sshah/Desktop/BME280_Environment_Sensor_Demo/BME280_demo/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_def.h:
+
+C:/Users/sshah/Desktop/BME280_Environment_Sensor_Demo/BME280_demo/Drivers/CMSIS/Device/ST/STM32F1xx/Include/stm32f1xx.h:
+
+C:/Users/sshah/Desktop/BME280_Environment_Sensor_Demo/BME280_demo/Drivers/CMSIS/Device/ST/STM32F1xx/Include/stm32f103xe.h:
+
+C:/Users/sshah/Desktop/BME280_Environment_Sensor_Demo/BME280_demo/Drivers/CMSIS/Include/core_cm3.h:
+
+C:/Users/sshah/Desktop/BME280_Environment_Sensor_Demo/BME280_demo/Drivers/CMSIS/Include/core_cmInstr.h:
+
+C:/Users/sshah/Desktop/BME280_Environment_Sensor_Demo/BME280_demo/Drivers/CMSIS/Include/cmsis_gcc.h:
+
+C:/Users/sshah/Desktop/BME280_Environment_Sensor_Demo/BME280_demo/Drivers/CMSIS/Include/core_cmFunc.h:
+
+C:/Users/sshah/Desktop/BME280_Environment_Sensor_Demo/BME280_demo/Drivers/CMSIS/Device/ST/STM32F1xx/Include/system_stm32f1xx.h:
+
+C:/Users/sshah/Desktop/BME280_Environment_Sensor_Demo/BME280_demo/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_rcc_ex.h:
+
+C:/Users/sshah/Desktop/BME280_Environment_Sensor_Demo/BME280_demo/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_gpio.h:
+
+C:/Users/sshah/Desktop/BME280_Environment_Sensor_Demo/BME280_demo/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_gpio_ex.h:
+
+C:/Users/sshah/Desktop/BME280_Environment_Sensor_Demo/BME280_demo/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_dma.h:
+
+C:/Users/sshah/Desktop/BME280_Environment_Sensor_Demo/BME280_demo/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_dma_ex.h:
+
+C:/Users/sshah/Desktop/BME280_Environment_Sensor_Demo/BME280_demo/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_cortex.h:
+
+C:/Users/sshah/Desktop/BME280_Environment_Sensor_Demo/BME280_demo/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash.h:
+
+C:/Users/sshah/Desktop/BME280_Environment_Sensor_Demo/BME280_demo/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash_ex.h:
+
+C:/Users/sshah/Desktop/BME280_Environment_Sensor_Demo/BME280_demo/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_pwr.h:
+
+C:/Users/sshah/Desktop/BME280_Environment_Sensor_Demo/BME280_demo/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_spi.h:
---
+++ BME280_Environment_Sensor_Demo/Debug/Src/subdir.mk
@@ -0,0 +1,62 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../Src/BME280.c \
+../Src/SSD1306.c \
+../Src/gpio.c \
+../Src/main.c \
+../Src/spi.c \
+../Src/stm32f1xx_hal_msp.c \
+../Src/stm32f1xx_it.c \
+../Src/system_stm32f1xx.c 
+
+OBJS += \
+./Src/BME280.o \
+./Src/SSD1306.o \
+./Src/gpio.o \
+./Src/main.o \
+./Src/spi.o \
+./Src/stm32f1xx_hal_msp.o \
+./Src/stm32f1xx_it.o \
+./Src/system_stm32f1xx.o 
+
+C_DEPS += \
+./Src/BME280.d \
+./Src/SSD1306.d \
+./Src/gpio.d \
+./Src/main.d \
+./Src/spi.d \
+./Src/stm32f1xx_hal_msp.d \
+./Src/stm32f1xx_it.d \
+./Src/system_stm32f1xx.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Src/BME280.o: C:/Users/sshah/Desktop/BME280_Environment_Sensor_Demo/BME280_demo/Src/BME280.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: MCU GCC Compiler'
+	@echo $(PWD)
+	arm-none-eabi-gcc -mcpu=cortex-m3 -mthumb -mfloat-abi=soft '-D__weak=__attribute__((weak))' '-D__packed=__attribute__((__packed__))' -DUSE_HAL_DRIVER -DSTM32F103xE -I"C:/Users/sshah/Desktop/BME280_Environment_Sensor_Demo/BME280_demo/Inc" -I"C:/Users/sshah/Desktop/BME280_Environment_Sensor_Demo/BME280_demo/Drivers/STM32F1xx_HAL_Driver/Inc" -I"C:/Users/sshah/Desktop/BME280_Environment_Sensor_Demo/BME280_demo/Drivers/STM32F1xx_HAL_Driver/Inc/Legacy" -I"C:/Users/sshah/Desktop/BME280_Environment_Sensor_Demo/BME280_demo/Drivers/CMSIS/Device/ST/STM32F1xx/Include" -I"C:/Users/sshah/Desktop/BME280_Environment_Sensor_Demo/BME280_demo/Drivers/CMSIS/Include"  -Og -g3 -Wall -fmessage-length=0 -ffunction-sections -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+Src/SSD1306.o: C:/Users/sshah/Desktop/BME280_Environment_Sensor_Demo/BME280_demo/Src/SSD1306.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: MCU GCC Compiler'
+	@echo $(PWD)
+	arm-none-eabi-gcc -mcpu=cortex-m3 -mthumb -mfloat-abi=soft '-D__weak=__attribute__((weak))' '-D__packed=__attribute__((__packed__))' -DUSE_HAL_DRIVER -DSTM32F103xE -I"C:/Users/sshah/Desktop/BME280_Environment_Sensor_Demo/BME280_demo/Inc" -I"C:/Users/sshah/Desktop/BME280_Environment_Sensor_Demo/BME280_demo/Drivers/STM32F1xx_HAL_Driver/Inc" -I"C:/Users/sshah/Desktop/BME280_Environment_Sensor_Demo/BME280_demo/Drivers/STM32F1xx_HAL_Driver/Inc/Legacy" -I"C:/Users/sshah/Desktop/BME280_Environment_Sensor_Demo/BME280_demo/Drivers/CMSIS/Device/ST/STM32F1xx/Include" -I"C:/Users/sshah/Desktop/BME280_Environment_Sensor_Demo/BME280_demo/Drivers/CMSIS/Include"  -Og -g3 -Wall -fmessage-length=0 -ffunction-sections -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+Src/%.o: ../Src/%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: MCU GCC Compiler'
+	@echo $(PWD)
+	arm-none-eabi-gcc -mcpu=cortex-m3 -mthumb -mfloat-abi=soft '-D__weak=__attribute__((weak))' '-D__packed=__attribute__((__packed__))' -DUSE_HAL_DRIVER -DSTM32F103xE -I"C:/Users/sshah/Desktop/BME280_Environment_Sensor_Demo/BME280_demo/Inc" -I"C:/Users/sshah/Desktop/BME280_Environment_Sensor_Demo/BME280_demo/Drivers/STM32F1xx_HAL_Driver/Inc" -I"C:/Users/sshah/Desktop/BME280_Environment_Sensor_Demo/BME280_demo/Drivers/STM32F1xx_HAL_Driver/Inc/Legacy" -I"C:/Users/sshah/Desktop/BME280_Environment_Sensor_Demo/BME280_demo/Drivers/CMSIS/Device/ST/STM32F1xx/Include" -I"C:/Users/sshah/Desktop/BME280_Environment_Sensor_Demo/BME280_demo/Drivers/CMSIS/Include"  -Og -g3 -Wall -fmessage-length=0 -ffunction-sections -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
